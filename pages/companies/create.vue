a999d1552cd1ebc795dcc0f94ae8265874652d94
unknown <ekobudi029@gmail.com>
2023-07-06 13:43:30 +0700

    create company

---
+++ pages/companies/create.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="py-[200px] flex flex-col items-center justify-center px-4">
+      <div class="text-[32px] font-semibold text-dark mb-4">Create Companies</div>
+        <div class="form-group">
+            <form class="w-full card"  @submit.prevent="createCompany">
+            <div class="form-group">
+                <label for="" class="text-grey" >Company Name</label>
+                <input type="text" class="input-field"  v-model="company.name">
+            </div>
+
+            <button type="submit" class="w-full btn btn-primary mt-[14px]">
+                Saves
+            </button>
+        </form>
+      </div>
+    </section>
+  </template>
+
+<script>
+export default{
+  middleware: 'auth',
+  data() {
+    return {
+        company: {
+            name: '',
+        },
+    }
+  },
+
+  methods: {
+   async createCompany() {
+        try {
+        this.$axios.post('/company', this.company)
+        .then(response => {
+            console.log(response)
+            this.$router.push({
+                name: 'companies-id',
+                params: {
+                    id: response.data.result.id,
+                },
+            })
+        })
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+  }
+}
+</script>
